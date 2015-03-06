1449f8a3115a1b4d3f32d641d1361c5098adfbc2
Mickhopes <mickael.turnel@gmail.com>
2015-03-06 01:34:10 +0100

    Ajout des fichiers manquants

---
+++ INC/struct.h
@@ -0,0 +1,41 @@
+/****************************************************
+ * L3 informatique
+ * Projet SAI
+ *
+ * Auteurs :  - Line Pouvaret
+ *            - Mickaël Turnel
+ *
+ * Fichier : struct.h
+ *
+ *****************************************************/
+
+#ifndef _STRUCT_
+#define _STRUCT_
+
+typedef struct Cell {
+	int MUR_HAUT;
+	int MUR_BAS;
+	int MUR_GAUCHE;
+	int MUR_DROITE;
+	int MUR_AVANT;
+	int MUR_ARRIERE;
+	int VISITED;
+}Cell;
+
+typedef struct Element {
+	Cell *cell;
+	struct Element *suivant;
+}Element;
+
+typedef struct Pile {
+	int nb_elem;
+	Element *debut;
+	Element *sommet;
+}Pile;
+
+typedef struct Maze {
+	int width, length, heigth;
+	Cell*** cases;
+}Maze;
+
+#endif /* _STRUCT_ */
