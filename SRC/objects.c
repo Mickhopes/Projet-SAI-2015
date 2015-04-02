326c8df1c034256e4ecadb479cc5db066a06000d
Mickhopes <mickael.turnel@gmail.com>
2015-04-02 11:43:37 +0200

    Ajout ascenceur et fonctions flèches de direction

--- SRC/objects.c
+++ SRC/objects.c
@@ -88,6 +88,7 @@
     for(z = 0; z < maze->width; z++){
         for(x = 0; x < maze->length; x++){
             for(y = 0; y < maze->height; y++){
+                /* On dessine le mur arrière */
                 if (maze->cases[z][x][y].MUR_ARRIERE == 1) {
                     glBegin(GL_QUADS);
                         glColor3f(0.9,0.9,0.9);
@@ -105,6 +106,7 @@
                         glVertex3f(x*TAILLE_CUBE+TAILLE_CUBE,y*TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
                     glEnd();
                 }
+                /* On dessine de mur de droite */
                 if (maze->cases[z][x][y].MUR_DROITE == 1) {
                     glBegin(GL_QUADS);
                         glColor3f(0.7,0.5,0.7);
@@ -122,7 +124,10 @@
                         glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
                     glEnd();
                 }
-                if (maze->cases[z][x][y].MUR_HAUT == 1) {
+                /* On dessine le mur du dessus tout le temps sauf si l'utilisateur de déplace
+                 * entre deux niveaux
+                 */
+                if (a.x != z || a.y != x || a.z != y) {
                     glBegin(GL_QUADS);
                         glColor3f(0.8,0.2,0.6);
                         glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE);
@@ -138,6 +143,32 @@
                         glVertex3f(x*TAILLE_CUBE+TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
                         glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
                     glEnd();
+                } else {
+                    if (a.en_mouvement == 0) {
+                        glBegin(GL_QUADS);
+                            glColor3f(0.8,0.2,0.6);
+                            glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE);
+                            glVertex3f(x*TAILLE_CUBE+TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE);
+                            glVertex3f(x*TAILLE_CUBE+TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
+                            glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
+                        glEnd();
+
+                        glBegin(GL_LINE_LOOP);
+                            glColor3f(0, 0, 0);
+                            glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE);
+                            glVertex3f(x*TAILLE_CUBE+TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE);
+                            glVertex3f(x*TAILLE_CUBE+TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
+                            glVertex3f(x*TAILLE_CUBE,y*TAILLE_CUBE+TAILLE_CUBE,z*TAILLE_CUBE+TAILLE_CUBE);
+                        glEnd();
+                    }
+                }
+                /* On dessine l'indicateur de montée */
+                if (maze->cases[z][x][y].MUR_HAUT == 0) {
+                    fleche_montee(z, x, y);
+                }
+                /* On dessine l'indicateur de descente */
+                if (maze->cases[z][x][y].MUR_BAS == 0) {
+                    fleche_descente(z, x, y);
                 }
             }
         }
@@ -145,3 +176,79 @@
 
     glLineWidth(1.0);
  }
+
+void fleche_montee(int x, int y, int z) {
+    int tx, ty, tz;
+    /* On dessinera si possible les indicateurs sur ce mur */
+    if (maze.cases[z][x][y].MUR_DROITE == 1) {
+        tz = x*TAILLE_CUBE+TAILLE_CUBE;
+        tx = y*TAILLE_CUBE+TAILLE_CUBE/2;
+        ty = z*TAILLE_CUBE+TAILLE_CUBE/2;
+
+        glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex3f(tz-0.01,tx-1.5,ty-3);
+            glVertex3f(tz-0.01,tx-1.5,ty);
+            glVertex3f(tz-0.01,tx+1.5,ty);
+            glVertex3f(tz-0.01,tx+1.5,ty-3);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex3f(tz-1.01,tx-1.5,ty-3);
+            glVertex3f(tz-1.01,tx-1.5,ty);
+            glVertex3f(tz-1.01,tx+1.5,ty);
+            glVertex3f(tz-1.01,tx+1.5,ty-3);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex3f(tz-0.01,tx-1.5,ty-3);
+            glVertex3f(tz-1.01,tx-1.5,ty-3);
+            glVertex3f(tz-1.01,tx-1.5,ty);
+            glVertex3f(tz-0.01,tx-1.5,ty);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex3f(tz-0.01,tx-1.5,ty-3);
+            glVertex3f(tz-1.01,tx-1.5,ty-3);
+            glVertex3f(tz-1.01,tx+1.5,ty-3);
+            glVertex3f(tz-0.01,tx+1.5,ty-3);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex3f(tz-0.01,tx+1.5,ty-3);
+            glVertex3f(tz-1.01,tx+1.5,ty-3);
+            glVertex3f(tz-1.01,tx+1.5,ty);
+            glVertex3f(tz-0.01,tx+1.5,ty);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex3f(tz-0.01,tx-1.5,ty);
+            glVertex3f(tz-1.01,tx-1.5,ty);
+            glVertex3f(tz-1.01,tx+1.5,ty);
+            glVertex3f(tz-0.01,tx+1.5,ty);
+        glEnd();
+    } else {
+        /* Sinon on prendra le mur suivant */
+        if (maze.cases[x][y][z].MUR_AVANT == 1) {
+
+        } else {
+            /* etc... */
+            if (maze.cases[x][y][z].MUR_GAUCHE == 1) {
+
+            } else {
+                if (maze.cases[x][y][z].MUR_ARRIERE == 1) {
+
+                }
+            }
+        }
+    }
+}
+
+void fleche_descente(int x, int y, int z) {
+
+}
