5a069b475f87ee793362d1785a311591c36c5f2f
LilyOfTheWest <linovergame@hotmail.fr>
2015-05-08 19:14:24 +0200

    Hélico ajouté + timer modifié

--- INC/objects.h
+++ INC/objects.h
@@ -21,5 +21,7 @@ void mettre_fleche_descente(int x, int y, int z);
 void fleche_montee();
 void fleche_descente();
 void helico();
+void para_rect(float x1, float y1, float z1, float x2, float y2, float z2);
+void hud();
 
 #endif /* _OBJECTS_ */
--- SRC/objects.c
+++ SRC/objects.c
@@ -402,35 +402,150 @@ void helico(){
 
     /* Coordonnées de base du centre du corps de l'hélico */
     int xH = (maze.width*TAILLE_CUBE)/2;
-    int yH = (maze.height*TAILLE_CUBE)+17;
+    int yH = (maze.height*TAILLE_CUBE)+11;
     int zH = (maze.length*TAILLE_CUBE)/2;
 
     /* Fuselage de l'hélico <=> ellipsoïde de centre xH, yH, zH */
+
     glPushMatrix();
     glTranslatef(xH,yH,zH);
-    glScalef(2.0,3.0,6.0);
-    glColor3f(0,0,1);
+    glScalef(2.0,1.5,3.0);
+    glColor3f(0.3,0.3,0.3);
     gluSphere(gluNewQuadric(), 5.0, 20, 20);
     glPopMatrix();
 
 
-    glPushMatrix();
+
     /* Train d'atterrissage */
 
+    float l = 1, h = 3, e = 1;
+    float xabs = 3.5, yabs = 6+h, zabs = 6;
+
+    glColor3f(0.2,0.2,0.2);
+    para_rect(xH-xabs,yH-yabs,zH-zabs,xH-xabs+l,yH-yabs+h, zH-zabs+e);
+    para_rect(xH+xabs,yH-yabs,zH-zabs,xH+xabs+l,yH-yabs+h,zH-zabs+e);
+    para_rect(xH-xabs,yH-yabs,zH+zabs,xH-xabs+l,yH-yabs+h,zH+zabs+e);
+    para_rect(xH+xabs,yH-yabs,zH+zabs,xH+xabs+l,yH-yabs+h,zH+zabs+e);
+
+
+    h = 1, e = 25;
+    yabs +=h,zabs +=e/2;
+
+    glColor3f(0.25,0.25,0.25);
+    para_rect(xH-xabs,yH-yabs,zH-zabs,xH-xabs+l,yH-yabs+h,zH+zabs);
+    para_rect(xH+xabs,yH-yabs,zH-zabs,xH+xabs+l,yH-yabs+h,zH+zabs);
+
     /* Mat */
+    h = 2.5,e = 1;
+    xabs = l/2,yabs=4+h,zabs = e/2;
+
+    glColor3f(0.35,0.35,0.35);
+    para_rect(xH-xabs,yH+yabs,zH-zabs,xH+xabs,yH+yabs+h,zH+zabs);
 
     /* Pales de rotor*/
 
+    h = 0.2, e = 50;
+    yabs+=2.5,zabs = e/2;
+
+    glColor3f(0.4,0.4,0.4);
+    para_rect(xH-xabs,yH+yabs,zH-zabs,xH+xabs,yH+yabs+h,zH+zabs);
+    l = e, e = 1;
+    xabs = zabs,zabs = 0.5;
+    para_rect(xH-xabs,yH+yabs,zH-zabs,xH+xabs,yH+yabs+h,zH+zabs);
+
+
     /* Queue */
+
+    l = 1, h = 1, e = 20;
+    xabs = l/2,yabs = h/2, zabs = 14;
+
+    glColor3f(0.35,0.35,0.35);
+    para_rect(xH-xabs,yH-yabs,zH+zabs,xH+xabs,yH+yabs,zH+zabs+e);
+
+    h = 2.5,e = 1;
+    zabs += 19;
+
+    glColor3f(0.3,0.3,0.3);
+    para_rect(xH-xabs,yH+yabs,zH+zabs,xH+xabs,yH+yabs+h,zH+zabs+e);
 
     /* Rotor de Queue */
 
+    glPushMatrix();
+    glTranslatef(-0.5,2.5,33.5);
+    glTranslatef(xH,yH,zH);
+    glRotatef(45.0,1.0,0.0,0.0);
+
+
+    glColor3f(0.4,0.4,0.4);
+
+
+    l = 0.2, h = 6, e = 0.7;
+    xabs = l/2,yabs = h/2, zabs = e/2;
+
+    para_rect(-xabs,-yabs,-zabs,xabs,yabs,zabs);
+
+    l = 0.2, h = 0.7, e = 6;
+    xabs = l/2,yabs = h/2,zabs = e/2 ;
+
+    para_rect(-xabs,-yabs,-zabs,xabs,yabs,zabs);
 
     glPopMatrix();
 
 
 
+}
 
+
+/* Fonction qui dessine un parallélépipède rectangle à partir de 2 points */
+void para_rect(float x1, float y1, float z1, float x2, float y2, float z2){
+
+    /* Face avant */
+    glBegin(GL_QUADS);
+    glVertex3f(x1,y1,z1);
+    glVertex3f(x2,y1,z1);
+    glVertex3f(x2,y2,z1);
+    glVertex3f(x1,y2,z1);
+    glEnd();
+
+    /* Face arrière */
+    glBegin(GL_QUADS);
+    glVertex3f(x1,y1,z2);
+    glVertex3f(x2,y1,z2);
+    glVertex3f(x2,y2,z2);
+    glVertex3f(x1,y2,z2);
+    glEnd();
+
+    /* Face gauche */
+    glBegin(GL_QUADS);
+    glVertex3f(x1,y1,z1);
+    glVertex3f(x1,y2,z1);
+    glVertex3f(x1,y2,z2);
+    glVertex3f(x1,y1,z2);
+    glEnd();
+
+    /* Face droite */
+    glBegin(GL_QUADS);
+    glVertex3f(x2,y1,z1);
+    glVertex3f(x2,y2,z1);
+    glVertex3f(x2,y2,z2);
+    glVertex3f(x2,y1,z2);
+    glEnd();
+
+    /* Face dessous */
+    glBegin(GL_QUADS);
+    glVertex3f(x1,y1,z1);
+    glVertex3f(x2,y1,z1);
+    glVertex3f(x2,y1,z2);
+    glVertex3f(x1,y1,z2);
+    glEnd();
+
+    /* Face dessus */
+    glBegin(GL_QUADS);
+    glVertex3f(x1,y2,z1);
+    glVertex3f(x2,y2,z1);
+    glVertex3f(x2,y2,z2);
+    glVertex3f(x1,y2,z2);
+    glEnd();
 }
 
 void hud(){
@@ -449,9 +564,14 @@ void hud(){
     int i;
     int min = timer / 60;
     int sec = timer % 60;
-    char time[5];
+    int dix_min = min / 10;
+    int unite_min = min % 10;
+    int dix_sec = sec / 10;
+    int unite_sec = sec % 10;
 
-    sprintf(time, "%d:%d", min, sec);
+    char time[6];
+
+    sprintf(time, "%d%d:%d%d", dix_min,unite_min,dix_sec,unite_sec);
 
     glLineWidth(2.0);
     glColor3f(1.0, 0.0, 0.0);
@@ -468,3 +588,4 @@ void hud(){
     glMatrixMode(GL_MODELVIEW);
     glPopMatrix();
 }
+
